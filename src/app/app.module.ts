19e07f5d6404f2faa33befffe67324cecf4f5c6c
Alan_Josue <alan_260499@hotmail.com>
2020-12-03 21:21:22 -0600

    Version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HelloWorld} from './componentes/primer.component';
+import {CalculadoraComponent} from './calculadora/calculadora.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorld,
+    CalculadoraComponent,
+    
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
